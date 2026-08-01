b082d64b903858155fb974d594cb71bd16ef5803
pausenplay <pauseandplayjnp@gmail.com>
2026-08-01 15:25:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let nextMemberID = 6007;
-function generateMemberID() {
-    let id = "26PP0" + nextMemberID;
+document.addEventListener("DOMContentLoaded", function () {
+    const member = document.getElementById("memberid");
+    if(member){
+        member.innerHTML="Member ID : 26PP0"+nextMemberID;
+    }
+});
+function saveMember(){
+    let member={
+        memberid:"26PP0"+nextMemberID,
+        name:document.getElementById("name").value,
+        mobile:document.getElementById("mobile").value,
+        plan:document.getElementById("plan").value,
+        startdate:document.getElementById("startdate").value
+    };
+    console.log(member);
+    alert("Member Saved Successfully");
     nextMemberID++;
-    return id;
 }
