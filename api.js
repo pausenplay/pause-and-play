6f234f93134e44da594b228901706144fa4a1267
pausenplay <pauseandplayjnp@gmail.com>
2026-08-01 20:13:18 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,102 +1,51 @@
 const API_URL = "YOUR_APPS_SCRIPT_WEB_APP_URL";
-
-
 async function apiGet(action, value=""){
-
 let url = API_URL + "?action=" + action;
-
 if(value!=""){
-
 url += "&memberid=" + value;
-
 }
-
 let response = await fetch(url);
-
 return await response.json();
-
 }
-
-
-
 async function apiPost(data){
-
 let response = await fetch(API_URL,{
-
 method:"POST",
-
 body:JSON.stringify(data)
-
 });
-
-
 return await response.json();
-
 }
-
-
-
 async function addMember(data){
-
 return await apiPost(data);
-
 }
-
-
-
 async function getMember(id){
-
 return await apiGet("getMember",id);
-
 }
-
-
-
 async function getMembers(){
-
 return await apiGet("getAllMembers");
-
 }
-
-
-
 async function updateMember(data){
-
 data.action="update";
-
 return await apiPost(data);
-
 }
-
-
-
 async function renewMember(data){
-
 data.action="renew";
-
 return await apiPost(data);
-
 }
-
-
-
 async function getDashboard(){
-
 return await apiGet("dashboard");
-
 }
 async function markAttendance(memberid){
-
 let response =
 await fetch(
-
 API_URL+
 "?action=attendance&memberid="
 +memberid
-
 );
-
-
 return await response.json();
-
+}
+async function deleteMember(id){
+return await apiPost({
+action:"delete",
+memberid:id
+});
 }
