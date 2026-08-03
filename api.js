02b48f8875abf381ac846ab897d3346631fd9929
pausenplay <pauseandplayjnp@gmail.com>
2026-08-03 17:55:15 +0530

    Update api.js

--- api.js
+++ api.js
@@ -2,8 +2,7 @@
 // Pause & Play API
 // =========================
 
-const API_URL = "YOUR_APPS_SCRIPT_WEB_APP_URL";
-
+const API_URL="https://script.google.com/macros/s/AKfycbwVVpKy9oZBYJGpN9u-iLn2tXF8b489ZceBbE_N1FFBs9mXvzCWiyaUcc0Mkm-qKcs9Uw/exec";
 
 // ---------- Common GET ----------
 
